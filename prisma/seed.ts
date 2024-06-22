e34f6d389614cca296b409d5d292a2bffa584439
unknown <amazighmalek.com>
2024-06-22 12:01:26 +0200

    seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,31 +1,31 @@
 import { PrismaClient } from '@prisma/client'
-import {faker} from '@faker-js/faker'
+// import {faker} from '@faker-js/faker'
 const prisma = new PrismaClient()
 
 async function main() {
 
   // generate fake data for user model
- await prisma.user.createMany({
-    data:Array.from({length:27}, () =>( {
-      email:faker.internet.email(),
-      name:faker.internet.userName(),
-      address:{
-        street:faker.location.streetAddress(),
-        city:faker.location.city(),
-        state:faker.location.state(),
-        zip:faker.location.zipCode()
-      },
-    }))
-})
+//  await prisma.user.createMany({
+//     data:Array.from({length:27}, () =>( {
+//       email:faker.internet.email(),
+//       name:faker.internet.userName(),
+//       address:{
+//         street:faker.location.streetAddress(),
+//         city:faker.location.city(),
+//         state:faker.location.state(),
+//         zip:faker.location.zipCode()
+//       },
+//     }))
+// })
 
-// generate fake data for todo model
+// // generate fake data for todo model
 
-await prisma.todo.createMany({
-  data:Array.from({length:27}, () =>( {
-    title:faker.person.firstName(),
-    body:faker.lorem.paragraph()
-  }))
-})
+// await prisma.todo.createMany({
+//   data:Array.from({length:27}, () =>( {
+//     title:faker.person.firstName(),
+//     body:faker.lorem.paragraph()
+//   }))
+// })
 
 }
 
